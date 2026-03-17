f6556c6d881b2e7254dc5f798aeafbe92c8d5e36
aryanpipersenia <aryanpipersenia@gmail.com>
2026-03-17 08:24:12 +0530

    feat: implement eureka discovery server on port 8761

---
+++ discovery-server/src/test/java/com/moviebooking/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviebooking.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
